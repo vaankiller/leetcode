a61917e81573d244f79744556f08f19b645facbc
vaan killer <534895367@qq.com>
2016-07-20 22:06:29 +0800

    add 98 tree.h

---
+++ tree/tree.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define bool int 
+#define true 1
+#define false 0
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+ };
+

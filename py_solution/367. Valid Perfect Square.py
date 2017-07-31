57c7c96a9a1c7db4d8e4ae674b5c91157093fe33
vaankiller <534895367@qq.com>
2017-07-31 14:48:52 +0800

    add 367.py

---
+++ py_solution/367. Valid Perfect Square.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def isPerfectSquare(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        from math import sqrt
+        return int(sqrt(num))**2 == num
